7fb7811522959229607112c16886722bdc6a0f66
Jose Daniel Cacioppo Armas <danielcacioppo@gmail.com>
2022-09-14 23:19:44 +0000

    proyect

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = ()=>{
+    return(
+        <footer className="footer">
+						copyright @our WebPage
+		</footer>
+    )
+}
+
+export default Footer
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,28 @@
 import React from "react";
-
-//include images into your bundle
+import Card from "./card.jsx";
 import rigoImage from "../../img/rigo-baby.jpg";
+//include images into your bundle
+
+import Jumbotron from "./jumbotron.jsx";
+import Navbar from "./navbar.jsx";
+import Footer from "./footer.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<Navbar></Navbar>
+			<div className="container"> 
+				<Jumbotron></Jumbotron>
+				<div className="cardSection d-sm-flex">
+					<Card></Card>
+					<Card></Card>
+					<Card></Card>
+					<Card></Card>
+				</div>
+				<Footer></Footer>
+			</div>
+		</>
 	);
 };
 
